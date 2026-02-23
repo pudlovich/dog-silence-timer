af1f015a926059dcdf79ac6c07138b057dc2b06f
Maciej Stanisz <macstanisz@gmail.com>
2026-02-23 22:59:14 +0100

    Major rework so that interval plan can be configured by picking separate min and max interval

--- silence-timer.js
+++ silence-timer.js
@@ -1,15 +1,134 @@
 // // //
-// // FUNCTIONS FOR LAUNCHING THE TIMER AT THE TOP LEVEL
-document.getElementById("startMeal").addEventListener("click", launchMealTimer);
-function launchMealTimer() {
-  setIntervalsArray([40,40,50,50,60,60,75,75,90,90,105,105,120])
+// // FUNCTIONS FOR DOM INTERACTION
+document.getElementById("start").addEventListener("click", launchTimer);
+function launchTimer() {
+  const minInt = intervalSelector.getMin();
+  const maxInt = intervalSelector.getMax();
+  // TODO error if max is lower than min
+  const intervalsArray = buildIntervalList(minInt, maxInt);
+  setIntervalsArray(intervalsArray);
+  console.log(intervalsArray);
+  // TODO show intervals Array in UI
   startTimer(getCurrentInterval(), onTimerEnd);
 }
 
-document.getElementById("startSilence").addEventListener("click", launchSilenceTimer);
-function launchSilenceTimer() {
-  setIntervalsArray([10,10,10,15,15,20,20,25,25,30,30,40,40,50,50,60])
-  startTimer(getCurrentInterval(), onTimerEnd);
+class IntervalSelector {
+  minColumnRoot = document.getElementById('min-interval-buttons');
+  maxColumnRoot = document.getElementById('max-interval-buttons');
+  availableMin = [
+    { val: 10, label: "0:10"},
+    { val: 15, label: "0:15"},
+    { val: 20, label: "0:20"},
+    { val: 30, label: "0:30"},
+    { val: 40, label: "0:40"},
+    { val: 60, label: "1:00"},
+  ];
+  availableMax = [
+    { val: 30, label: "0:30"},
+    { val: 40, label: "0:40"},
+    { val: 60, label: "1:00"},
+    { val: 80, label: "1:20"},
+    { val: 100, label: "1:40"},
+    { val: 120, label: "2:00"},
+  ];
+  selectedMin = 20;
+  selectedMax = 60;
+
+  constructor() {
+    this.setUpButtons();
+    this.selectMin(20);
+    this.selectMax(60);
+  }
+
+  getMin() {
+    return this.selectedMin;
+  }
+
+  getMax() {
+    return this.selectedMax;
+  }
+
+  selectMin(val) {
+    this.selectedMin = val;
+    this.unselectMin();
+    this.selectButton(`min${val}`);
+  }
+
+  selectMax(val) {
+    this.selectedMax = val;
+    this.unselectMax();
+    this.selectButton(`max${val}`);
+  }
+
+  setUpButtons() {
+    if(this.minColumnRoot) {
+      this.availableMin.forEach((minVal) => {
+        const button = document.createElement('button');
+        button.textContent = minVal.label;
+        button.id = `min${minVal.val}`;
+        button.className = 'min-interval-button';
+        this.minColumnRoot.appendChild(button);
+        button.addEventListener('click', () => this.selectMin(minVal.val));
+      });
+    }
+    if(this.maxColumnRoot) {
+      this.availableMax.forEach((maxVal) => {
+        const button = document.createElement('button');
+        button.textContent = maxVal.label;
+        button.id = `max${maxVal.val}`;
+        button.className = 'max-interval-button';
+        this.maxColumnRoot.appendChild(button);
+        button.addEventListener('click', () => this.selectMax(maxVal.val));
+      });
+    }
+  }
+
+  unselectMin() {
+    document.querySelectorAll('.min-interval-button').forEach(b => b.classList.remove('active'));
+  }
+  unselectMax() {
+    document.querySelectorAll('.max-interval-button').forEach(b => b.classList.remove('active'));
+  }
+  selectButton(id) {
+    const btn = document.getElementById(id);
+    if(btn) {
+      btn.classList.add('active');
+    }
+    console.log(`current min: ${this.getMin()}, current max: ${this.getMax()}`)
+  }
+}
+
+const intervalSelector = new IntervalSelector();
+
+// // //
+// // FUNCTIONS FOR BUILDING THE INTERVALS ARRAY BASED ON SELECTED MIN MAX VALUES
+// 15 values total, first going by small steps (each repeated), then by large ones
+// // small interval for delta<50 is 10% of delta; for delta<80 it's 5, else it's 10
+// // large interval is a third of what remains
+function buildIntervalList(minVal, maxVal) {
+  const delta = maxVal - minVal;
+  const smallStep = calculateSmallStep(delta);
+  const largeStep = Math.floor((delta - (4 * smallStep)) / 3);
+
+  return [minVal, minVal,
+    minVal + smallStep, minVal + smallStep,
+    minVal + 2*smallStep, minVal + 2*smallStep,
+    minVal + 3*smallStep, minVal + 3*smallStep,
+    minVal + 4*smallStep, minVal + 4*smallStep,
+    maxVal - 2*largeStep, maxVal - 2*largeStep,
+    maxVal - largeStep, maxVal - largeStep,
+    maxVal
+  ]
+}
+
+function calculateSmallStep(delta) {
+  if (delta < 50) {
+    return Math.floor(delta * 0.1);
+  } else if (delta < 80) {
+    return 5;
+  } else {
+    return 10;
+  }
 }
 
 // // //
